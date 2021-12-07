184f07149a2f69c61b02b08754e4dd5c9c68a62f
isaac1212 <nsambai722@gmail.com>
2021-12-07 16:42:28 +0300

    auth

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,7 +29,7 @@ class AuthServiceProvider extends ServiceProvider
         // if (! $this->app->routesAreCached()) {
             Passport::routes();
 
-            Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
+            Passport::loadKeysFrom('/oauth');
         // }
 
         //
