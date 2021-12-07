5e96be29e359c5f22d98872749428d7c8035ce0c
isaac1212 <nsambai722@gmail.com>
2021-12-07 18:23:30 +0300

    post

--- routes/api.php
+++ routes/api.php
@@ -50,7 +50,7 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::post('profile_edit_image', [UserAuthController::class, 'profile_edit_image']); 
     Route::get('/profile',[UserAuthController::class, 'profile']); // done
     Route::post('add_address', [UserAuthController::class, 'add_address']); 
-    Route::apiResource('products', ProductController::class);
+    Route::post('product', [ProductController::class, 'store']);
 
 
     
