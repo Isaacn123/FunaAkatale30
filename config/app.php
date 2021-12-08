88c106a641513d955388f75f2f3f6a85ee2939a3
isaac1212 <nsambai722@gmail.com>
2021-12-09 00:28:02 +0300

    env uploadcare

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        PedZed\LaravelUploadcare\UploadcareServiceProvider::class,
 
         /*
          * Package Service Providers...
