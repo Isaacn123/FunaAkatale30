5ff4750855f9d08cf6063a0422dec64f56d72c27
isaac1212 <nsambai722@gmail.com>
2021-12-08 21:48:15 +0300

    home

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,4 @@ Route::resource('dash-b', DashboardController::class);
 
 // Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
