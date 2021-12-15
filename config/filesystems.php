7d712f54c406da26eaeea46c682e62c07ca8407d
isaac1212 <nsambai722@gmail.com>
2021-12-15 12:59:07 +0300

    local

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'uploadcare'),
+    'default' => env('FILESYSTEM_DRIVER', 'local'),
 
     /*
     |--------------------------------------------------------------------------
